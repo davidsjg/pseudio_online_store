8002ac0f56d2d44fd1936bc5fb8d5d818c727d36
Joseph Davidson <j.davidson18@gmail.com>
2025-01-01 14:56:42 -0700

    fixed hover image change for left header

--- src/Header/LeftHeader/LeftHeader.js
+++ src/Header/LeftHeader/LeftHeader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./LeftHeader.css";
-import logo from "./pseudio.png";
+import logo from "./logo.png";
+import logo2 from './logo2.png'
 import homePic1 from "./Home.png";
 import home2 from './Home2.png'
 import productPic1 from "./Products.png";
@@ -12,52 +13,53 @@ import about2 from './About2.png'
 import {useState} from "react"
 
 function Header(props) {
+  const [logoPic, setLogoPic] = useState(logo);
   const [discountPic, setDiscountPic] = useState(discPic);
   const [productPic, setProductPic] = useState(productPic1);
   const [homePic, setHomePic] = useState(homePic1);
   const [aboutPic, setAboutPic] = useState(about);
   let myBool = false;
 
+  const mouseEnter9 = () => {
+    setLogoPic(logo2);
+  };
+  const mouseLeave9 = () => {
+    setLogoPic(logo);
+  };
+
   const mouseEnter1 = () => {
     setHomePic(home2);
-
   };
-
   const mouseLeave1 = () => {
     setHomePic(homePic1);
-
   };
+
   const mouseEnter2 = () => {
     setProductPic(product2);
-
   };
-
   const mouseLeave2 = () => {
     setProductPic(productPic1);
-
   };
+
   const mouseEnter3 = () => {
     setDiscountPic(discount2);
-
   };
-
   const mouseLeave3 = () => {
     setDiscountPic(discPic);
-
   };
+
   const mouseEnter4 = () => {
     setAboutPic(about2);
   };
-
   const mouseLeave4 = () => {
     setAboutPic(about);
-
   };
+
   return (
     <>
       <div className="mainLeft">
  
-          <img className="mlImage" src={logo} alt="" />
+          <img className="mlImage" src={logoPic} alt="" onMouseEnter={mouseEnter9} onMouseLeave={mouseLeave9} />
   
           <img className="mlImage2 adjPic" src={homePic} alt="" onMouseEnter={mouseEnter1} onMouseLeave={mouseLeave1} />
      
